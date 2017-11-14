0f255446b777a8ce51553071f91ad9781799b061
Adam Fudge <coolfudge@gmail.com>
2017-11-14 23:05:05 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,9 +8,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
+    Show.order(rating: :asc).take.rating
   end
 
   def self.least_popular_show
+    Show.order(rating: :asc).take
   end
 
   def self.ratings_sum
